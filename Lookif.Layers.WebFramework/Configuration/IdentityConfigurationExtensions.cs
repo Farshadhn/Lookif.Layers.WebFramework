464aa28f85db0d9e2efd6550b7720da35ddd4eec
Farshad <sfarshadhn@outlook.com>
2022-02-21 12:39:56 +0330

    IdentityConfigurationExtensions support initial identityOptions

--- Lookif.Layers.WebFramework/Configuration/IdentityConfigurationExtensions.cs
+++ Lookif.Layers.WebFramework/Configuration/IdentityConfigurationExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Lookif.Library.Common;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection; 
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System;
 using Lookif.Layers.Core.MainCore.Identities;
@@ -9,32 +9,35 @@ namespace Lookif.Layers.WebFramework.Configuration
 {
     public static class IdentityConfigurationExtensions
     {
-        public static void AddCustomIdentity<T>(this IServiceCollection services, IdentitySettings settings)  
+        public static void AddCustomIdentity<T>(
+            this IServiceCollection services,
+            IdentitySettings settings,
+            Action<IdentityOptions> identityOptions = null
+            )
          where T : IdentityDbContext<User, Role, Guid>
         {
-            services.AddIdentity<User, Role>(identityOptions =>
+            if (identityOptions is not null)
             {
-                //Password Settings
-                identityOptions.Password.RequireDigit = settings.PasswordRequireDigit;
-                identityOptions.Password.RequiredLength = settings.PasswordRequiredLength;
-                identityOptions.Password.RequireNonAlphanumeric = settings.PasswordRequireNonAlphanumic; //#@!
-                identityOptions.Password.RequireUppercase = settings.PasswordRequireUppercase;
-                identityOptions.Password.RequireLowercase = settings.PasswordRequireLowercase;
+                services.AddIdentity<User, Role>(identityOptions)
+                    .AddEntityFrameworkStores<T>()
+                    .AddDefaultTokenProviders();
+            }
+            else
+            {
+                services.AddIdentity<User, Role>(identityOptions =>
+                {
+                    //Password Settings
+                    identityOptions.Password.RequireDigit = settings.PasswordRequireDigit;
+                    identityOptions.Password.RequiredLength = settings.PasswordRequiredLength;
+                    identityOptions.Password.RequireNonAlphanumeric = settings.PasswordRequireNonAlphanumic; //#@!
+                    identityOptions.Password.RequireUppercase = settings.PasswordRequireUppercase;
+                    identityOptions.Password.RequireLowercase = settings.PasswordRequireLowercase;
 
-                //UserName Settings
-                identityOptions.User.RequireUniqueEmail = settings.RequireUniqueEmail;
+                    //UserName Settings
+                    identityOptions.User.RequireUniqueEmail = settings.RequireUniqueEmail;
 
-                //Singin Settings
-                //identityOptions.SignIn.RequireConfirmedEmail = false;
-                //identityOptions.SignIn.RequireConfirmedPhoneNumber = false;
-
-                //Lockout Settings
-                //identityOptions.Lockout.MaxFailedAccLookif.LayersAttempts = 5;
-                //identityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                //identityOptions.Lockout.AllowedForNewUsers = false;
-            })
-            .AddEntityFrameworkStores<T>()
-            .AddDefaultTokenProviders();
+                }).AddEntityFrameworkStores<T>().AddDefaultTokenProviders();
+            }
         }
     }
 }
